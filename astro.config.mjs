84f850833da1647cfd32fee1d34fb5157d88417b
Marco <gautier24@gmail.com>
2024-12-12 16:09:54 +0100

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://m-gautier.fr/blog',
   integrations: [mdx(), sitemap(), react(), icon()],
   adapter: vercel(),
   output: 'server',
